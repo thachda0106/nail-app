f9cdcf55be386ebed2284169aaaa7b12f2455400
Thach Da <thachbovjp@gmail.com>
2024-07-13 10:39:50 +0700

    first commit

---
+++ shared/layouts/Main.layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+interface PropsType {
+  children: React.ReactNode;
+}
+const MainLayout: React.FC<PropsType> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  )
+}
+
+export default MainLayout
---
+++ shared/layouts/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <header>
+      <span>Footer</span>
+    </header>
+  )
+}
+
+export default Footer
