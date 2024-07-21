3830c74644eef8940f68a769731e7d96531cab31
phambao7320 <hoangbaongoc7320@gmail.com>
2024-07-21 18:06:31 +0700

    add logo

--- shared/constants/storeInformation.ts
+++ shared/constants/storeInformation.ts
@@ -1,10 +1,12 @@
+import type { Metadata } from "next";
 import { TIME_WORKING } from "./calendar";
 
 export const STORE_INFORMATION = {
   Location: {
     name: "Floral Crush Beauty",
     address: "4093 rue principale, Tracadie, NB, Canada, New Brunswick",
-    description: 'Enjoy personalized treatments and meticulous care designed to leave you feeling rejuvenated and beautiful.',
+    description:
+      "Enjoy personalized treatments and meticulous care designed to leave you feeling rejuvenated and beautiful.",
   },
   Contacts: {
     phoneNumber: "+1 506-210-4191",
@@ -18,17 +20,17 @@ export const STORE_INFORMATION = {
   },
 };
 
-
-export const META_DATA =  { 
+export const META_DATA: Metadata = {
   title: STORE_INFORMATION.Location.name,
   description: STORE_INFORMATION.Location.description,
-  keywords: 'Floral Crush Beauty, manicure, hand care, nail care, premium services, beauty treatments, rejuvenation',
-  author: STORE_INFORMATION.Location.name,
+  keywords:
+    "Floral Crush Beauty, manicure, hand care, nail care, premium services, beauty treatments, rejuvenation",
+  authors: { name: STORE_INFORMATION.Location.name },
   openGraph: {
     title: STORE_INFORMATION.Location.name,
-    type: 'website',
-    url: 'https://nail-app.thachda0106.com',
-    image: 'https://nail-app.thachda0106.com/assets/images/logo-black.png',
+    type: "website",
+    url: "https://nail-app.thachda0106.com",
+    images: "https://nail-app.thachda0106.com/assets/images/logo-black.png",
     description: STORE_INFORMATION.Location.description,
-  }
+  },
 };
