426c1d47e750ee989099152c7605b8afe1ef74ac
thachnguyen <114375783+thach0106@users.noreply.github.com>
2024-07-30 13:14:59 +0700

    feat: config csp

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,33 @@
 /** @type {import('next').NextConfig} */
+
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' fonts.googleapis.com 'unsafe-inline';
+  img-src 'self' blob: data:;
+  connect-src 'self';
+  media-src 'self';
+  font-src 'self';
+  object-src 'self';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'self';
+`;
+
 const nextConfig = {
+  headers() {
+    return [
+      {
+        source: '/:path*', 
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ]
+  },
   images: {
     deviceSizes: [480, 768, 1024, 1280, 1440],
   },
@@ -12,6 +40,7 @@ const nextConfig = {
      * - favicon.ico (favicon file)
      */
     '/((?!api|_next/static|_next/image|favicon.ico).*)',
+
   ],
 };
 
